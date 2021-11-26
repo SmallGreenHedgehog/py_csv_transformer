4306b323596f9e112e489bf451dff07bb8c10463
mr_v <v.about.work@gmail.com>
2021-11-26 04:48:19 +0300

    Добавит преобразователь из csv лид конвертера в csv для звонопса
    Реализован не полностью, см TODO

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class BASECSVFileExtractor:
         """Сеттер пути результирующей директории"""
         self._dst_dir_path = dst_dir_path
 
-    def __init__(self, src_file_path, dst_dir_path):
+    def __init__(self, src_file_path, dst_dir_path, *args, **kwargs):
         self._init_dialect()
         self.set_file_path(src_file_path=src_file_path)
         self.set_dst_dir_path(dst_dir_path=dst_dir_path)
@@ -69,10 +69,54 @@ def get_paths_of_files_in_source_folder() -> tuple:
     return tuple(f'{source_dir_path}/{cur_file}' for cur_file in files_in_src_dir)
 
 
+class LeadConvToRingerDogCSVFileExtractor(BASECSVFileExtractor):
+    _reusable_field_names_list = []
+
+    def set_reusable_field_names_list(self, reusable_field_names_list):
+        """Метод для установки переносимых полей из исходного csv в результирующий"""
+        self._reusable_field_names_list = reusable_field_names_list
+
+    def __init__(self, src_file_path, dst_dir_path, reusable_field_names_list=[], *args, **kwargs):
+        super(LeadConvToRingerDogCSVFileExtractor, self).__init__(src_file_path, dst_dir_path, *args, **kwargs)
+        self.set_reusable_field_names_list(reusable_field_names_list=reusable_field_names_list)
+
+    def _prepare_phone_number_sting(self, src_phone_str):
+        """Готовит телефон для звонопса"""
+        # TODO реализовать функционал подготовки телефона для звонопса
+        return src_phone_str
+
+    def _append_reusable_fields_to_new_dict(self, cur_dict, new_dict):
+        """Переносит переиспользуемые поля из исходного csv в результирующий"""
+        for cur_field_name in self._reusable_field_names_list:
+            new_dict[cur_field_name] = cur_dict.get(cur_field_name)
+
+    def _prepare_data_in_list_of_dict(self):
+        """Метод для подготовки данных из лид конвертера для звонопса"""
+        prepared_csv_file_list_of_dict = []
+        for cur_dict in self._csv_file_list_of_dict:
+            new_dict = {}
+
+            self._append_reusable_fields_to_new_dict(cur_dict=cur_dict, new_dict=new_dict)
+            new_dict['phone'] = self._prepare_phone_number_sting(cur_dict.get('phone'))
+
+            prepared_csv_file_list_of_dict.append(new_dict)
+            self._csv_file_list_of_dict = prepared_csv_file_list_of_dict
+
+
 if __name__ == '__main__':
     try:
         dst_dir_path = abspath(getcwd() + '/result_csv_files')
+        NeededExtractor = LeadConvToRingerDogCSVFileExtractor
+
         for cur_file_path in get_paths_of_files_in_source_folder():
-            BASECSVFileExtractor(src_file_path=cur_file_path, dst_dir_path=dst_dir_path).extract_data_to_result_file()
+            NeededExtractor(
+                src_file_path=cur_file_path,
+                dst_dir_path=dst_dir_path,
+                reusable_field_names_list=[
+                    'first_name', 'last_name', 'channel_name',
+                    'utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'
+                ],
+            ).extract_data_to_result_file()
+
     except Exception as e:
         print(f'Ошибка выполнения обработки конвертации: {e}')
