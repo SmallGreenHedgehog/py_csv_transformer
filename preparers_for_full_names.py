3af3965f4dc5b865db0ee9e61f28585aa594b21b
mr_v <v.about.work@gmail.com>
2021-12-22 17:59:12 +0300

    Добавит класс подготовки полного имени для платформы Давида

--- preparers_for_full_names.py
+++ preparers_for_full_names.py
@@ -1,4 +1,8 @@
 from abc import abstractmethod
+from os import getcwd
+from os.path import abspath
+
+from helpers_file_list_checker import ListCheckerFromCSVNamesFile
 
 
 class FullName:
@@ -6,6 +10,7 @@ class FullName:
 
     _src_full_name = ''
     _without_extra_spaces_full_name = ''
+
     _first_name = ''
     _middle_name = ''
     _last_name = ''
@@ -23,14 +28,15 @@ class FullName:
         """
         self._without_extra_spaces_full_name = ' '.join(self._src_full_name.replace('\t', ' ').split())
 
-    def __init__(self, src_full_name: str):
-        self.set_src_full_name(src_full_name=src_full_name)
-        self._init_without_extra_spaces_full_name()
-
     @abstractmethod
     def prepare_separate_parts_of_fullname(self):
         """Абстрактный метод для подготовки отдельных частей имени в наследниках"""
         pass
+
+    def __init__(self, src_full_name: str, *args, **kwargs):
+        self.set_src_full_name(src_full_name=src_full_name)
+        self._init_without_extra_spaces_full_name()
+        self.prepare_separate_parts_of_fullname()
 
     @property
     def first_name(self):
@@ -40,9 +46,47 @@ class FullName:
     @property
     def middle_name(self):
         """Возвращает отчество, приготовленное на базе полного имени"""
-        return self._first_name
+        return self._middle_name
 
     @property
     def last_name(self):
         """Возвращает имя, приготовленное на базе полного имени"""
-        return self._first_name
+        return self._last_name
+
+
+class FullNameFromDavidPlatform(FullName):
+    _nature_list_checker = None
+
+    def _init_nature_list_checker(self):
+        nature_names_file_path = abspath(getcwd() + '/csv_dict_helpers/nature_names.csv')
+        self._nature_list_checker = ListCheckerFromCSVNamesFile(
+            src_file_path=nature_names_file_path,
+            column_name='Name',
+        )
+
+    def __init__(self, *args, **kwargs):
+        self._init_nature_list_checker()
+        super(FullNameFromDavidPlatform, self).__init__(*args, **kwargs)
+
+    def _check_first_name(self, first_name):
+        """Проверяет имя по справочнику"""
+        return self._nature_list_checker.check_value(first_name)
+
+    def _prepare_first_name(self):
+        """Готовит свойство имени"""
+        full_name_words_list = self._without_extra_spaces_full_name.split()
+        first_word = full_name_words_list[0] if len(full_name_words_list) > 0 else ''
+        self._first_name = first_word if self._check_first_name(first_word) else self._without_extra_spaces_full_name
+
+    def _prepare_last_name(self):
+        """Готовит свойство фамилии
+
+        Присваивает свойству _last_name
+        оставшуюся часть полного имени,
+        после удаления _first_name"""
+        self._last_name = self._without_extra_spaces_full_name.replace(self._first_name, '').strip()
+
+    def prepare_separate_parts_of_fullname(self):
+        """Готовит отдельные поля имени и фамилии"""
+        self._prepare_first_name()
+        self._prepare_last_name()
